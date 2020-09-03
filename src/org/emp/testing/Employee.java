68e7b6821acd8c3f74ec0c2f8fe5f17d2027be1c
yuvarajrathinam <yuvarajrathinam62@gmail.com>
2020-09-03 21:52:42 +0530

    done

---
+++ src/org/emp/testing/Employee.java
@@ -0,0 +1,36 @@
+package org.emp.testing;
+
+public class Employee {
+private void empId() {
+System.out.println("empid is 154");
+}
+private void empName() {
+System.out.println("empname is yuvi");
+}
+private void empDob() {
+System.out.println("emp dob is ");
+}
+private void emphno() {
+	// TODO Auto-generated method stub
+System.out.println("emphone is 12563");
+}
+private void empEmail() {
+System.out.println("emp email is yuva");
+
+}
+private void empAdress() {
+	// TODO Auto-generated method stub
+System.out.println("emp address is ");
+}
+public static void main(String[] args) {
+	Employee v = new Employee();
+	v.empId();
+	v.empName();
+	v.empDob();
+	v.emphno();
+	v.empAdress();
+			
+}
+}
+		
+
---
+++ src/org/emp/testing/Employees.java
@@ -0,0 +1,31 @@
+package org.emp.testing;
+
+public class Employees {
+private void empId() {
+	// TODO Auto-generated method stub
+System.out.println("empId is 66464");
+
+}
+private void empName() {
+	// TODO Auto-generated method stub
+System.out.println("empName is yuvaraj");
+
+}
+private void empDob() {
+	// TODO Auto-generated method stub
+System.out.println("empDob is ");
+
+}
+private void empPhone() {
+	// TODO Auto-generated method stub
+System.out.println("empphno is ");
+
+}
+public static void main(String[] args) {
+	Employees v = new Employees();
+	v.empDob();
+	v.empId();
+	v.empName();
+	v.empPhone();
+}
+}
